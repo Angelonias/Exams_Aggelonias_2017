3f64b9125f840ebde04269d3448b2fc6f1a23be7
Angelonias <aggeloniasdev@gmail.com>
2017-01-27 10:36:23 +0200

    Initial

---
+++ app/src/main/java/aggelonias/exams/android/exams_aggelonias/MainActivity.java
@@ -0,0 +1,13 @@
+package aggelonias.exams.android.exams_aggelonias;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
